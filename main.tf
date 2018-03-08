37282cf1e8dd7afbdb34c3e43143d088200b5bba
Arvind Gorur <arvind.gorur@hotmail.com>
2018-03-08 14:48:53 -0500

    Added resource group and virtual network configuration.

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ provider "azurerm" {
 	subscription_id = "${var.Subscription_ID}"
 	tenant_id       = "${var.Tenant_ID}"
 }
+
+resource "azurerm_resource_group" "rg-terraform-east" {
+	name		= "rg-terraform-east"
+	location	= "Canada East"
+}
+
+resource "azurerm_virtual_network" "production-network" {
+    name = "production-network"
+		address_space = ["10.1.0.0/16"]
+		location = "${azurerm_resource_group.rg-terraform-east.location}"
+		resource_group_name = "${azurerm_resource_group.rg-terraform-east.name}"
+}
