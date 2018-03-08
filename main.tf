875cff125b2c48b1b6446c1e1075ae58c5555b2e
Arvind Gorur <arvind.gorur@hotmail.com>
2018-03-08 15:21:05 -0500

    Cleanup

--- main.tf
+++ main.tf
@@ -5,13 +5,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg-terraform-east" {
-	name		= "rg-terraform-east"
+	name			= "rg-terraform-east"
 	location	= "Canada East"
 }
 
 resource "azurerm_virtual_network" "production-network" {
-    name = "production-network"
-		address_space = ["10.1.0.0/16"]
-		location = "${azurerm_resource_group.rg-terraform-east.location}"
+    name 								= "production-network"
+		address_space 			= ["10.1.0.0/16"]
+		location 						= "${azurerm_resource_group.rg-terraform-east.location}"
 		resource_group_name = "${azurerm_resource_group.rg-terraform-east.name}"
+}
+
+resource "azurerm_network_security_group" "my-security-group" {
+	name = "my-security-group"
+	location = "${azurerm_resource_group.rg-terraform-east.location}"
+	resource_group_name = "${azurerm_resource_group.rg-terraform-east.name}"
 }
