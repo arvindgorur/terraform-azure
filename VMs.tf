1d06ec603c5c45d4a0d080b3b1c4ad311af44b04
Arvind Gorur <arvind.gorur@hotmail.com>
2018-03-09 14:44:54 -0500

    A fully running infrastructure with VM!

---
+++ VMs.tf
@@ -0,0 +1,37 @@
+resource "azurerm_virtual_machine" "ubuntu-server" {
+  name 								  = "ubuntu-server"
+	location 						  = "${azurerm_resource_group.rg-terraform-east.location}"
+	resource_group_name   = "${azurerm_resource_group.rg-terraform-east.name}"
+  network_interface_ids = ["${azurerm_network_interface.ubuntu-server-nic.id}"]
+	vm_size 						  = "Standard_A1"
+
+	#delete_on_termination = true
+	#delete_data_disks_on_termination = true
+
+	storage_image_reference {
+		publisher = "Canonical"
+		offer 		= "UbuntuServer"
+		sku 			= "16.04-LTS"
+		version 	= "latest"
+	}
+
+  storage_os_disk {
+    name = "ubuntu-os"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name = "ubuntu-server"
+    admin_username = "arvindgorur"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path = "/home/arvindgorur/.ssh/authorized_keys"
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrXptbJa4DVMP046NDT5vM+rdLtM/WO9lap5OzhRnSReMS+THIvpBhOS1dpH5LQYMpNb/Cvs4uBao596cuXWieL8JBkqRR9qDhvlJv22p7wNfh9ynJ1bE5siSWmtUucyAuRMl4hf2u38ZKwp0K6SdsN8TdvgydX+Ze+4bZgHhS+LKLi0g+HrOhHKVutdEPsdz842wtgBlPygVCCIDMLCnvj2MFZ7Cm48fb39uMCHVr88Cgspgj6CgehS78qNo36wXC8s195EReaPZKz+TxfUMktVnm6dPY+4t42xcvlJNcaTJZ703dr32McedQtgb0aYeMeUMH/ohcC2U3ScILmIiT imported-openssh-key"
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -9,34 +9,6 @@ resource "azurerm_resource_group" "rg-terraform-east" {
 	location	= "Canada East"
 }
 
-resource "azurerm_virtual_network" "production-network" {
-	name 								= "production-network"
-	address_space 			= ["10.1.0.0/16"]
-	location 						= "${azurerm_resource_group.rg-terraform-east.location}"
-	resource_group_name = "${azurerm_resource_group.rg-terraform-east.name}"
-}
-
-resource "azurerm_subnet" "YYZ" {
-	name 									= "YYZ"
-	resource_group_name 	= "${azurerm_resource_group.rg-terraform-east.name}"
-	virtual_network_name 	= "${azurerm_virtual_network.production-network.name}"
-	address_prefix 				= "10.1.1.0/24"
-}
-
-resource "azurerm_network_interface" "test" {
-  name = "VNetInterface1"
-	location 									= "${azurerm_resource_group.rg-terraform-east.location}"
-	resource_group_name 			= "${azurerm_resource_group.rg-terraform-east.name}"
-	network_security_group_id = "${azurerm_network_security_group.my-security-group.id}"
-
-	ip_configuration {
-		name 													= "TestConfiguration1"
-		subnet_id 										= "${azurerm_subnet.YYZ.id}"
-		private_ip_address_allocation = "static"
-		private_ip_address 						= "10.1.1.25"
-	}
-}
-
 resource "azurerm_network_security_group" "my-security-group" {
 	name 								= "my-security-group"
 	location 						= "${azurerm_resource_group.rg-terraform-east.location}"
@@ -55,4 +27,70 @@ resource "azurerm_network_security_rule" "allow-ssh" {
 	destination_address_prefix 	= "*"
 	resource_group_name 				= "${azurerm_resource_group.rg-terraform-east.name}"
   network_security_group_name = "${azurerm_network_security_group.my-security-group.name}"
+}
+
+resource "azurerm_network_security_rule" "allow-http" {
+	name 												= "allow HTTP"
+	priority 										= 110
+	direction 									= "Inbound"
+	access 											= "Allow"
+	protocol 										= "TCP"
+	source_port_range 					= "*"
+	destination_port_range 			= "80"
+	source_address_prefix 			= "*"
+	destination_address_prefix 	= "*"
+	resource_group_name 				= "${azurerm_resource_group.rg-terraform-east.name}"
+  network_security_group_name = "${azurerm_network_security_group.my-security-group.name}"
+}
+
+resource "azurerm_network_security_rule" "allow-https" {
+	name 												= "allow HTTPS"
+	priority 										= 120
+	direction 									= "Inbound"
+	access 											= "Allow"
+	protocol 										= "TCP"
+	source_port_range 					= "*"
+	destination_port_range 			= "443"
+	source_address_prefix 			= "*"
+	destination_address_prefix 	= "*"
+	resource_group_name 				= "${azurerm_resource_group.rg-terraform-east.name}"
+  network_security_group_name = "${azurerm_network_security_group.my-security-group.name}"
+}
+
+resource "azurerm_virtual_network" "production-network" {
+	name 								= "production-network"
+	address_space 		= ["10.1.0.0/16"]
+	location 			= "${azurerm_resource_group.rg-terraform-east.location}"
+	resource_group_name = "${azurerm_resource_group.rg-terraform-east.name}"
+}
+
+resource "azurerm_subnet" "prod-subnet" {
+	name 											= "prod-subnet"
+	resource_group_name 			= "${azurerm_resource_group.rg-terraform-east.name}"
+	virtual_network_name 			= "${azurerm_virtual_network.production-network.name}"
+	address_prefix 			  		= "10.1.1.0/24"
+	network_security_group_id = "${azurerm_network_security_group.my-security-group.id}"
+}
+
+resource "azurerm_network_interface" "ubuntu-server-nic" {
+  name = "ubuntu-server-nic"
+	location 									= "${azurerm_resource_group.rg-terraform-east.location}"
+	resource_group_name 			= "${azurerm_resource_group.rg-terraform-east.name}"
+	network_security_group_id = "${azurerm_network_security_group.my-security-group.id}"
+
+	ip_configuration {
+		name 													= "ubuntu-server-nic-config"
+		subnet_id 										= "${azurerm_subnet.prod-subnet.id}"
+		private_ip_address_allocation = "static"
+		private_ip_address 						= "10.1.1.25"
+		public_ip_address_id 					= "${azurerm_public_ip.ubuntu-server.id}"
+	}
+}
+
+resource "azurerm_public_ip" "ubuntu-server" {
+	name = "ubuntu-server-ip"
+	location 										 = "${azurerm_resource_group.rg-terraform-east.location}"
+	resource_group_name 				 = "${azurerm_resource_group.rg-terraform-east.name}"
+	public_ip_address_allocation = "dynamic"
+	domain_name_label 					 = "ubuntu-server"
 }
