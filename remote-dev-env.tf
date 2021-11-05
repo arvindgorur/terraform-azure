52cd51c994e803c92b4b2be3ea236b2219e6a677
Arvind Gorur <agorur@hoopp.com>
2021-11-05 14:08:57 -0400

    Add NSG

--- remote-dev-env.tf
+++ remote-dev-env.tf
@@ -19,3 +19,21 @@ resource "azurerm_subnet" "default_subnet" {
   virtual_network_name = azurerm_virtual_network.remote_dev_env_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "main_nsg" {
+  name                = "main-nsg"
+  location            = azurerm_resource_group.remote_dev_env.location
+  resource_group_name = azurerm_resource_group.remote_dev_env.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
